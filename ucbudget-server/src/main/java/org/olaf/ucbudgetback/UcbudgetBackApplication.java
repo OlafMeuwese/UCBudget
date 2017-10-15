af0898c45f21ddb512a846290af313e91f28f4cd
Olaf Meuwese <omeuwese@bol.com>
2017-10-15 22:24:54 +0200

    Original initiation containing three parts of the project 1. Backend Server in Java Spring 2. Front-end in typescript with Angular 3. Folder for database (Postgres)

---
+++ ucbudget-server/src/main/java/org/olaf/ucbudgetback/UcbudgetBackApplication.java
@@ -0,0 +1,12 @@
+package org.olaf.ucbudgetback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcbudgetBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcbudgetBackApplication.class, args);
+	}
+}
